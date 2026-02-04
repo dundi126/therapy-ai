f04750d88aec1c8d4986bdeee4e3aeea93e27eb6
dundi126 <dundi126@gmail.com>
2026-02-04 17:56:44 -0500

    Add authentication functionality with better-auth integration
    - Updated package.json and package-lock.json to include better-auth dependency
    - Implemented auth client in src/lib/auth-client.ts
    - Created auth handler in src/lib/auth.ts
    - Added user registration form in src/app/page.tsx
    - Defined user, session, account, and verification tables in src/db/schema.ts
    - Added API route for authentication in src/app/api/auth/[...all]/route.ts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,37 @@
+"use client"
+
+
 import { Button } from "@/components/ui/button"
+import { Input } from "@base-ui/react"
+import { useState } from "react"
+import { authClient } from "@/lib/auth-client";
 
 export default function Home() {
+  const [name, setName] = useState("")
+  const [password, setPassword] = useState("")
+  const [email, setEmail] = useState("")
+
+  const onSubmit = () => authClient.signUp.email({
+        email, 
+        password, 
+        name, 
+    }, {
+        onSuccess: (ctx) => {
+            window.alert("Success")
+        },
+        onError: (ctx) => {
+            window.alert(`something went wrong: ${ctx.error.message}`)
+        },
+});
+
+
   return (
-    <div className="text-4xl font-bold text-green-500">
+    <div className="p-4 flex flex-col gap-4">
       <h1>Welcome to the Home Page</h1>
-      <Button variant="custom">Click Me</Button>
+      <Input placeholder="name" value={name} onChange={e => setName(e.target.value)} />
+      <Input placeholder="email" value={email} onChange={e => setEmail(e.target.value)} />
+      <Input placeholder="password" type="password" value={password} onChange={ e => setPassword(e.target.value)} />
+      <Button onClick={onSubmit}>Create User</Button>
     </div>
   )
 }
